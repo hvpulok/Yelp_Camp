b0b1f1c526721afa411a48f990599e141903da70
Kamrul <hvpulok@gmail.com>
2016-06-21 21:54:02 +0000

    Remove hardcoded campground array, add function to retrieve campground data from MongoDB and render in campground route, add function to add new campground in MongoDB

--- app.js
+++ app.js
@@ -18,26 +18,19 @@ var campgroundSchema = new mongoose.Schema({
 // define mongoose campground model based on schema
 var campground = mongoose.model("campground", campgroundSchema);
 
-var campgroundData = [
-        {name: "Camp 1", image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg" },
-        {name: "Camp 2", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" },
-        {name: "Camp 3", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg" },
-        {name: "Camp 4", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" },
-        {name: "Camp 5", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg" },
-        {name: "Camp 1", image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg" },
-        {name: "Camp 2", image: "https://farm3.staticflickr.com/2464/3694344957_14180103ed.jpg" },
-        {name: "Camp 3", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg" },
-        {name: "Camp 4", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg" },
-        {name: "Camp 5", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg" }
-        
-    ];
-
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgroundData:campgroundData});
+    //get all campgrounds from dB
+    campground.find({}, function(err, allCampgroundData){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgroundData:allCampgroundData});
+        }
+    });
 });
 
 app.get("/addCampground", function(req, res) {
@@ -48,9 +41,18 @@ app.post("/addCampground",function(req,res){
     var newCampgroundName = req.body.campgroundName;
     var newcampgroundImageURL = req.body.campgroundImageURL;
     var newCampground = {name: newCampgroundName, image: newcampgroundImageURL};
-    campgroundData.push(newCampground);
-    res.redirect("campgrounds");
     
+    // store in MongoDB
+    campground.create(newCampground, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Newly created Campground: ");    
+            console.log(campground);
+        }
+    });
+
+    res.redirect("campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(req, res){
