4925f297548ad5fc7b37fe4536a0b0f183f28370
Kamrul <hvpulok@gmail.com>
2016-07-08 23:40:00 +0000

    comment authorization to edit/ delete based on owner + show buttons accordingly

--- routes/comments.js
+++ routes/comments.js
@@ -45,6 +45,41 @@ router.post("/", isLoggedIn,function(req, res){
     })
 });
 
+// comment Edit route
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+    var selectedCampground = req.params.id;
+    var selectedComment = req.params.comment_id;
+    campground.findById(selectedCampground).populate("comments").exec(function(err, selectedCampground){
+        if(err){
+            console.log(err);
+        }else {
+            comment.findById(selectedComment, function(err, selectedComment) {
+                if(err){
+                    console.log(err);
+                    res.redirect("back");
+                } else{
+                    console.log(selectedComment);
+                    res.render("comments/edit", {selectedCampground:selectedCampground, selectedComment:selectedComment});                    
+                }
+            })
+        }
+    });
+});
+
+// Comment post/ PUT route
+router.put("/:comment_id/", checkCommentOwnership, function(req, res){
+    comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedCommentData){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else{
+            console.log(updatedCommentData);
+            res.redirect("/campgrounds/"+ req.params.id);
+        }
+    })
+});
+
+
 // define middleware to check user already loggedin or not
 // which can be used to protect viewing pages from unlogged users
 function isLoggedIn(req, res, next){
@@ -52,6 +87,28 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+// to check ownership of selected comment
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        //find the selected campground
+        comment.findById(req.params.comment_id, function(err, selectedComment) {
+            if(err){
+                console.log(err);
+                res.redirect("back"); // redirect to previous page
+            }else{
+                // check creator and current user is same or not
+                if(selectedComment.author.id.equals(req.user.id)){
+                    return next();
+                } else{
+                    res.redirect("back"); // redirect to previous page
+                }
+            }
+        });
+    } else{
+        res.redirect("/login"); // redirect to login page
+    }
 }
 
 module.exports = router;
